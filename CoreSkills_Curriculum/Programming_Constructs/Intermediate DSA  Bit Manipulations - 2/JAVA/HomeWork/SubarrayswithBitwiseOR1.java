31ca65291d1e29808c6b7988a6b2775e89b4db4b
Sarat Chandra Vanam <59186077+SaratVanam@users.noreply.github.com>
2022-09-12 10:42:41 +0530

    Optimized the code

--- CoreSkills_Curriculum/Programming_Constructs/Intermediate DSA  Bit Manipulations - 2/JAVA/HomeWork/SubarrayswithBitwiseOR1.java
+++ CoreSkills_Curriculum/Programming_Constructs/Intermediate DSA  Bit Manipulations - 2/JAVA/HomeWork/SubarrayswithBitwiseOR1.java
@@ -1,20 +1,14 @@
-// O(N) TC
+//O(N) TC
 
 public class Solution {
     public long solve(int A, int[] B) {
-        long zeroCount=0L;
-        long ans =(1L*A*(A+1))/2L;
+        long ans=0;
+        long last=-1;
         for(int i=0;i<A;i++){
-            if(B[i]==0){
-                zeroCount++;
+            if(B[i]==1){
+                last=i;
             }
-            else{
-                ans-=((1L*zeroCount)*(zeroCount+1))/2L;
-                zeroCount=0;
-            }
-        }
-        if(zeroCount>0){
-            ans-=(1L*(zeroCount)*(zeroCount+1))/2L;
+            ans+=last+1;
         }
         return ans;
     }
