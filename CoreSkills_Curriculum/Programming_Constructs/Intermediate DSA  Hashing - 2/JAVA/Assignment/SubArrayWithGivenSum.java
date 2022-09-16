cdbb0946f999f6e0ad6630e4865480d36db65053
Sarat Chandra Vanam <59186077+SaratVanam@users.noreply.github.com>
2022-09-16 14:00:45 +0530

    Optimized the code

--- CoreSkills_Curriculum/Programming_Constructs/Intermediate DSA  Hashing - 2/JAVA/Assignment/SubArrayWithGivenSum.java
+++ CoreSkills_Curriculum/Programming_Constructs/Intermediate DSA  Hashing - 2/JAVA/Assignment/SubArrayWithGivenSum.java
@@ -2,43 +2,34 @@
 
 public class Solution {
     public int[] solve(int[] A, int B) {
-        HashMap<Long,Integer> map = new HashMap<>();
-        long[] psum=new long[A.length];
-        long Z= (long) B;
-        psum[0]=A[0];
-        map.put(psum[0],0);
-        for(int i=1;i<A.length;i++){
-            psum[i]=psum[i-1]+A[i];
-            if(!map.containsKey(psum[i])){
-                map.put(psum[i],i);
-            }
-        }
-        int l=-1;
-        int r=-1;
-        for(int i=0;i<psum.length;i++){
-            long a =psum[i];
-            long b=a-Z;
-            if(psum[i]==Z){
-                l=0;
-                r=i;
+        int s=0,e=0;
+        int sum=0;
+        while(e<A.length){
+            sum+=A[e];
+            if(sum == B){
                 break;
             }
-            if(map.containsKey(b)){
-                l=map.get(b)+1;
-                r=i;
-                break;
+            else if(sum>B){
+                while(sum>B){
+                    sum-=A[s];
+                    s++;
+                }
+                if(sum==B){
+                    break;
+                }
             }
+            e++;
         }
-        if(l==-1){
+        if(e==A.length){
             return new int[]{-1};
         }
-        int len=r-l+1;
-        int[] ans= new int[len];
-        int x=0;
-        for(int i=l;i<=r;i++){
-            ans[x]=A[i];
-            x++;
+        int[] ans = new int[e-s+1];
+        int counter=0;
+        for(int i=s;i<=e;i++){
+            ans[counter]=A[i];
+            counter++;
         }
         return ans;
     }
 }
+
